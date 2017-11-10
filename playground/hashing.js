7312a272213dcd2d90cadff28f0bd645a16e4d50
Humberto <betoncios@gmail.com>
2017-11-10 16:41:39 -0200

    Add post users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,15 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+	id: 10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(token);
+
+var decoded = jwt.verify(token, '123abc');
+
+console.log(decoded);
+
+// console.log(SHA256("this is a test").toString());
